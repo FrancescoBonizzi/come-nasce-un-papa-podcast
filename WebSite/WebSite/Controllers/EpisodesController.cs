1a9580d7cf3baa06d53e8d1b984241e16fb535de
Francesco Bonizzi <fbonizzi.90@gmail.com>
2022-02-09 07:52:57 +0100

    Aggiunta la pagina del primo episodio

---
+++ WebSite/WebSite/Controllers/EpisodesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSite.Controllers
+{
+    public class EpisodesController : Controller
+    {
+        [Route(template: "/episodi/episodio-1-divento-papa")]
+        public IActionResult Episodio1()
+            => View();
+    }
+}
